5614539d9b70a2d03ec924fd5efc8276c9525e8c
Joseph Robert Spencer <joseph.spencer@cern.ch>
2020-02-26 12:32:29 +0100

    file changes

--- scope_main.py
+++ scope_main.py
@@ -1,5 +1,7 @@
 import sys
 import matplotlib
+import numpy as np 
+
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
@@ -33,7 +35,12 @@ class Window(QtWidgets.QMainWindow, ScopeGuiDesign.Ui_MainWindow):
         self.pushButton.clicked.connect(self.GetDataButton)
         self.pushButton_2.clicked.connect(self.ScopeConnect)
         self.pushButton_ClearPlot.clicked.connect(self.clearPlot)
-        
+        self.checkBox_5.stateChanged.connect(self.CheckBoxGrid)
+        self.pushButton_saveData.clicked.connect(self.saveFile)
+
+        self.storedData = []
+        self.fileNames = []
+
     def EmbedPlot(self):
 
         self.sc = MplCanvas(self, width=5, height=4, dpi=100)
@@ -45,25 +52,41 @@ class Window(QtWidgets.QMainWindow, ScopeGuiDesign.Ui_MainWindow):
         self.widget.setLayout(self.layout)
 
     def clearPlot(self):
+        self.storedData = []
         self.sc.axes.clear()
         self.sc.draw_idle()
 
+    def CheckBoxGrid(self):
+        if self.checkBox_5.isChecked():
+            self.sc.axes.grid(True)
+            self.sc.draw_idle()
+        else:
+            self.sc.axes.grid(False)
+            self.sc.draw_idle()
+
     def GetDataButton(self):
 
-        C1_state = self.checkBox.isChecked()       
-        C2_state = self.checkBox_2.isChecked()  
-        C3_state = self.checkBox_3.isChecked()  
-        C4_state = self.checkBox_4.isChecked()
+        C1_state = self.checkBox_CH1.isChecked()       
+        C2_state = self.checkBox_CH2.isChecked()  
+        C3_state = self.checkBox_CH3.isChecked()  
+        C4_state = self.checkBox_CH4.isChecked()
 
         channelStates = [C1_state, C2_state, C3_state, C4_state]
-
-        channel_code = ['C1', 'C2', 'C3', 'C4']
+        channel_code  = ['C1', 'C2', 'C3', 'C4']
 
         for i, channel in enumerate(channelStates):
             if channel == True:
                 data = self.scope.getData(channel_code[i])
-                self.sc.axes.plot(data[0],data[1])
+                self.sc.axes.plot(data[0],data[1], label = channel_code[i])
+                self.storedData.append(data)
+                self.fileNames.append(channel_code[i])
+        # for i, channel in zip(channel_code, channelStates):
+        #     if channel == True:
+        #         data = self.scope.getData(i)
+        #         self.sc.axes.plot(data[0],data[1], label = i)
 
+
+        self.sc.axes.legend()
         self.sc.draw_idle()
 
     def ScopeConnect(self):
@@ -75,6 +98,13 @@ class Window(QtWidgets.QMainWindow, ScopeGuiDesign.Ui_MainWindow):
         '''
         add an LED or notifier that the host has been connected to 
         '''
+
+    def saveFile(self):
+        directory = QtWidgets.QFileDialog.getExistingDirectory()
+
+        for name, dat in zip(self.fileNames, self.storedData):
+            np.save(directory+'/'+ name, dat )
+
 
 
 
